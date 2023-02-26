3c8c6c7b57fe3dcb1fe05b352fd52b20327d7708
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2023-02-26 14:57:30 +0200

    edit: - sign in functionality removed for testing purposes - info text added to sign in modal

--- scripts/api/loginAndShowCards.js
+++ scripts/api/loginAndShowCards.js
@@ -1,38 +1,38 @@
-import instance from "./instance.js";
-import showHideTxtAndBttns from "../utils/showHideTxtAndBttns.js";
-import getCards from "./getCards.js";
-import showCards from "../functions/showCards.js";
-import showHideFilter from "../functions/showHideFilter.js";
-import { cardsContainer } from "../variables/variables.js";
+import instance from './instance.js';
+import showHideTxtAndBttns from '../utils/showHideTxtAndBttns.js';
+import getCards from './getCards.js';
+import showCards from '../functions/showCards.js';
+import showHideFilter from '../functions/showHideFilter.js';
+import { cardsContainer } from '../variables/variables.js';
 
 async function loginAndShowCards() {
-  const signInInputCollection = document.querySelectorAll(".signin-modal__form-input");
-  const body = {};
+  // const signInInputCollection = document.querySelectorAll(".signin-modal__form-input");
+  // const body = {};
 
-  signInInputCollection.forEach((input) => {
-    body[input.name] = input.value;
-  });
+  // signInInputCollection.forEach((input) => {
+  //   body[input.name] = input.value;
+  // });
 
   try {
-    const { status, data } = await instance.post("/login", body);
-    if (status === 200) {
-      localStorage.setItem("token", data);
-      this.close();
-      const cards = await getCards();
-      cardsContainer.innerHTML = "";
-      showCards(cards);
-      showHideFilter(cards);
-      showHideTxtAndBttns(cards);
-    }
+    // const { status, data } = await instance.post('/login', body);
+    // if (status === 200) {
+    localStorage.setItem('token', '93db13d3-b950-4c8c-9fc6-06a6add26031');
+    this.close();
+    const cards = await getCards();
+    cardsContainer.innerHTML = '';
+    showCards(cards);
+    showHideFilter(cards);
+    showHideTxtAndBttns(cards);
+    // }
   } catch (err) {
     const {
       response: { status },
     } = err;
     if (status === 401) {
       console.warn(`${status}, Incorrect username or password.`);
-      document.querySelector(".signin-form__error-text").classList.remove("d-none");
-      document.querySelectorAll(".signin-modal__form-input").forEach((item) => {
-        item.classList.add("is-invalid");
+      document.querySelector('.signin-form__error-text').classList.remove('d-none');
+      document.querySelectorAll('.signin-modal__form-input').forEach((item) => {
+        item.classList.add('is-invalid');
       });
     } else {
       console.warn(err);
